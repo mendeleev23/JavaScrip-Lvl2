fd4c5262576fe624439714589be325f78848bead
mendeleev23 <58812293+mendeleev23@users.noreply.github.com>
2021-02-17 16:05:58 +0300

    Added webserver

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const fs = require('fs')
-const server = http.createServer((req, res) => {
-  console.log(req.url)
-  const body = fs.readFileSync(`./public/index.html`)
-  res.end(body)
-})
-server.listen(process.env.PORT || 5000)
-console.log('Server started')
